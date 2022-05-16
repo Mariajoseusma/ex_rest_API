f6762a383ede11de934dfcf7636932b430929127
DESKTOP-RIE7DOC\LENOVO <mariajoseusma@hotmail.com>
2022-05-16 11:32:47 -0500

    feat: destroy method was implemented

--- app/Controllers/Http/ProyectoController.js
+++ app/Controllers/Http/ProyectoController.js
@@ -9,14 +9,27 @@ class ProyectoController {
         return await user.proyectos().fetch();        
     }
 
-    async create({auth, request}){
+    async create({ auth, request }){
         const user = await auth.getUser();
         const { nombre } = request.all();
         const proyecto = new Proyecto();
         proyecto.fill({
             nombre
         });
-        await user.proyectos().save(proyecto)
+        await user.proyectos().save(proyecto);
+        return proyecto;
+    }
+
+    async destroy({ auth, response, params }){
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        if (proyecto.user_id !== user.id){
+            return Response.status(403).json({
+                mensaje : "Usted no es dueño de este proyecto por lo tanto no lo puede eliminar"
+            })
+        }
+        await proyecto.delete();
         return proyecto;
     }
 }
